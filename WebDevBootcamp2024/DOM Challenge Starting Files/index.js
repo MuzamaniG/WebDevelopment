b18402979af3588c08151a3c32e57468e20fa3db
mhgausi <mhgausi@ncsu.edu>
2024-12-21 21:57:38 -0500

    DOM challenge 2

--- WebDevBootcamp2024/DOM Challenge Starting Files/index.js
+++ WebDevBootcamp2024/DOM Challenge Starting Files/index.js
@@ -1,3 +1,2 @@
-document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Nope";
 
-document.firstElementChild.lastElementChild.querySelector("ul").firstElementChild.innerHTML = "Angela";
+document.querySelector("button").style.backgroundColor = "yellow";
