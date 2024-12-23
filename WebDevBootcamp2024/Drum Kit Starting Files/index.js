2062bb866bb999fa42ee9719ad366494d6349d7c
mhgausi <muzamanigausi@gmail.com>
2024-12-22 21:28:21 -0500

    Added background images to drum kit

--- WebDevBootcamp2024/Drum Kit Starting Files/index.js
+++ WebDevBootcamp2024/Drum Kit Starting Files/index.js
@@ -3,6 +3,6 @@ const buttons = document.querySelectorAll(".drum");
 // Adds clicked notification to each button
 for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function() {
-        alert("Clicked yes!");
+        new Audio("./sounds/tom-1.mp3").play();
     })
 }
