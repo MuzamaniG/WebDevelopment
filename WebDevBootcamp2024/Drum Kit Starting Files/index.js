477305f08f357cf6fc90d68563e8745994dad502
mhgausi <muzamanigausi@gmail.com>
2024-12-22 21:06:45 -0500

    Added commits, changed email for commits

--- WebDevBootcamp2024/Drum Kit Starting Files/index.js
+++ WebDevBootcamp2024/Drum Kit Starting Files/index.js
@@ -1,5 +1,6 @@
 const buttons = document.querySelectorAll(".drum");
 
+// Adds clicked notification to each button
 for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function() {
         alert("Clicked yes!");
