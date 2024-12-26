c8e82ec9c8c44288db59f793b72c8688f281cc09
mhgausi <muzamanigausi@gmail.com>
2024-12-26 16:32:03 -0500

    Added button animations

--- WebDevBootcamp2024/Drum Kit Starting Files/index.js
+++ WebDevBootcamp2024/Drum Kit Starting Files/index.js
@@ -6,11 +6,14 @@ for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function() {
         var buttonInnerHTML = this.innerHTML;
         makeSound(buttonInnerHTML);
+
+        buttonAnimation(buttonInnerHTML);
     });
 }
 
 document.addEventListener("keydown", function(event) {
     makeSound(event.key);
+    buttonAnimation(event.key);
 })
 
 function makeSound(key) {
@@ -46,4 +49,13 @@ function makeSound(key) {
             break;
         default: console.log(this.innerHTML);
     }
+}
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100);
 }
