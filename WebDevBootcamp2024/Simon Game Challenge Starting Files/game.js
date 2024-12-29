fae978c0c413c3d9954abf3b19c214146e3cf28b
mhgausi <muzamanigausi@gmail.com>
2024-12-29 18:52:37 -0500

    Added button click

--- WebDevBootcamp2024/Simon Game Challenge Starting Files/game.js
+++ WebDevBootcamp2024/Simon Game Challenge Starting Files/game.js
@@ -1,6 +1,7 @@
 
 var buttonColors = ["red", "blue", "green", "yellow"];
-var gamePattern = new Array();
+var gamePattern = [];
+var userClickedPattern = [];
 
 
 /** Decides the next sequence */
@@ -8,4 +9,19 @@ function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColor = buttonColors[randomNumber]
     gamePattern.push(randomChosenColor);
+
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    var audio = new Audio("./sounds/" + randomChosenColor + ".mp3");
+    audio.play();
 }
+
+$(".btn").click(function() {
+    var userChosenColor = this.attr("id");
+    userClickedPattern.push(userChosenColor);
+    console.log(userClickedPattern);
+}) 
+
+
+
+
+
