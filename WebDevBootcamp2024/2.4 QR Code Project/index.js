2b397721bc887b49c22d724cd83b14fee7b18bf5
MuzamaniG <muzamanigausi@gmail.com>
2025-01-03 08:14:49 -0500

    Added in prompts

--- WebDevBootcamp2024/2.4 QR Code Project/index.js
+++ WebDevBootcamp2024/2.4 QR Code Project/index.js
@@ -3,3 +3,22 @@
 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 3. Create a txt file to save the user input using the native fs node module.
 */
+
+import inquirer from 'inquirer';
+import qr from 'qr-image';
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then((answers) => {
+    qr.image(answers);
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      console.log("Prompt couldn't be rendered in the current environment");
+    } else {
+        console.log("Fail");
+    }
+  });
+
