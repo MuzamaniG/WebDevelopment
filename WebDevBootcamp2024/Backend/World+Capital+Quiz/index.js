eb4b380b3163c8c23d2f0adfe0eb8e9fc7b41a00
MuzamaniG <muzamanigausi@gmail.com>
2025-02-11 15:05:11 -0500

    Integrated Postgres with World Capitals Quiz

--- WebDevBootcamp2024/Backend/World+Capital+Quiz/index.js
+++ WebDevBootcamp2024/Backend/World+Capital+Quiz/index.js
@@ -1,14 +1,41 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg"
+import dotenv from 'dotenv'
 
 const app = express();
 const port = 3000;
+
+// Load the environment variables from "password.env
+dotenv.config({ path: './password.env' });
+
+console.log(process.env.PG_PASSWORD);
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: process.env.PG_PASSWORD,
+  port: 5432,
+  
+})
+
+db.connect();
 
 let quiz = [
   { country: "France", capital: "Paris" },
   { country: "United Kingdom", capital: "London" },
   { country: "United States of America", capital: "New York" },
 ];
+
+db.query("SELECT * FROM capitals", (err, res) => {
+  if (err) {
+    console.error("Error executing query", err.stack);
+  } else {
+    quiz = res.rows;
+  }
+  db.end();
+});
 
 let totalCorrect = 0;
 
