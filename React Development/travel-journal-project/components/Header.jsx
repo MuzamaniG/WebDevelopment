128d3a4d508cbf1bd75c37e5b9f85e2ac3d98698
MuzamaniG <muzamanigausi@gmail.com>
2025-03-31 23:39:21 -0400

    Created header

--- React Development/travel-journal-project/components/Header.jsx
+++ React Development/travel-journal-project/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <header>
-
+            <h1><img src="../src/assets/globe.png" id="globe-image"/>my travel journal.</h1>
         </header>
     )
 }
