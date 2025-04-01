828a9fe191401bd057d9c2b6d8876943d817fdb9
MuzamaniG <muzamanigausi@gmail.com>
2025-03-31 23:15:17 -0400

    Begun travel journal project

---
+++ React Development/travel-journal-project/src/App.jsx
@@ -0,0 +1,11 @@
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"));
+
+export default function App() {
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
