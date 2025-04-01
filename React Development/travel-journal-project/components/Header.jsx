f63a1a1751454b611570a672502f99e067a21f16
MuzamaniG <muzamanigausi@gmail.com>
2025-04-01 12:23:37 -0400

    Added alt text

--- React Development/travel-journal-project/components/Header.jsx
+++ React Development/travel-journal-project/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <header>
-            <h1><img src="../src/assets/globe.png" id="globe-image"/>my travel journal.</h1>
+            <h1><img src="../src/assets/globe.png" id="globe-image" alt="globe icon"/>my travel journal.</h1>
         </header>
     )
 }
