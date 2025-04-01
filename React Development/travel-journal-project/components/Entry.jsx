4ac67c148c9c7f5e3ce0ef2afd07b434857055d8
MuzamaniG <muzamanigausi@gmail.com>
2025-04-01 15:28:21 -0400

    Removed excessive div tags

--- React Development/travel-journal-project/components/Entry.jsx
+++ React Development/travel-journal-project/components/Entry.jsx
@@ -17,25 +17,21 @@
 export default function Entry() {
     return (
         <article>
-            <div className="entry-image">
+            <div className="entry-image-container">
                 <img src="https://scrimba.com/links/travel-journal-japan-image-url" alt="mount fuji" className="location-image"/>
             </div>
 
             <div className="entry">
+                <img src="../src/assets/marker.png" alt="marker" className="icon"/>
+                <span className="country-header">JAPAN</span>
 
-                <div className="country-header">
-                    <h2><img src="../src/assets/marker.png" alt="marker" className="icon"/>JAPAN</h2>
-                </div>
+                <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu" className="map">View in Google Maps</a>
 
-                <div className="map">
-                    <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">View in Google Maps</a>
-                </div>
+                <h2 className="location">Mount Fuji</h2>
 
-                <div className="location"><h1>Mount Fuji</h1></div>
+                <h2 className="dates">12 Jan, 2021 - 24 Jan, 2021</h2>
 
-                <div className="dates"><h2>12 Jan, 2021 - 24 Jan, 2021</h2></div>
-
-                <div className="description"><p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p></div>
+                <p className="description">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
             </div>
         </article>
     )
