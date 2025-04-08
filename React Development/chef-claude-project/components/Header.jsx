6d9683b4a8316f758bc5b2b6f2f82c707d2238d9
MuzamaniG <muzamanigausi@gmail.com>
2025-04-08 14:59:56 -0400

    Finished chef header component

---
+++ React Development/chef-claude-project/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header>
+            <img src="../images/chef-claude-icon.png" alt="chef-icon"></img>
+            <h1>
+                Chef Claude
+            </h1>
+        </header>
+    )
+}
