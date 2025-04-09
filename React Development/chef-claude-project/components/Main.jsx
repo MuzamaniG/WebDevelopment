a2bab479527026c8f1bd8c35ec0a259cce039cd8
MuzamaniG <muzamanigausi@gmail.com>
2025-04-09 19:08:15 -0400

    Added event listener for adding ingredients

--- React Development/chef-claude-project/components/Main.jsx
+++ React Development/chef-claude-project/components/Main.jsx
@@ -1,14 +1,40 @@
+
+import { useState } from "react";
+
+
 export default function Main() {
+
+    const [ingredients, setIngredients] = useState([]);
+    const ingredientListItems = ingredients.map(ingredient =>
+        <li key={ingredient}>{ingredient}</li>
+    )
+
+    function handleSubmit(event) {
+
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+
+        setIngredients(prevIngredients =>
+            [...prevIngredients, newIngredient]
+        )
+
+    }
     return (
         <main>
-            <form className="add-ingredient-form">
+            <form className="add-ingredient-form" onSubmit={handleSubmit}>
                 <input
                     type="text"
                     placeholder="e.g. Oregano"
                     aria-label="Add ingredient"
+                    name="ingredient"
                 />
                 <button>Add ingredient</button>
             </form>
+            <ul>
+                {ingredientListItems}
+            </ul>
+
         </main>
 
     )
