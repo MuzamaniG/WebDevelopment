2d9730566c730ea45ae08a4c4f78d8c4c5fb7f9c
MuzamaniG <muzamanigausi@gmail.com>
2025-05-03 17:05:43 -0400

    Moved ingredients list and placeholder recipe to separate components

--- React Development/chef-claude-project/components/Main.jsx
+++ React Development/chef-claude-project/components/Main.jsx
@@ -1,31 +1,9 @@
 
 import { useState } from "react";
-import ClaudeRecipe from "ClaudeRecipe.jsx"
+import ClaudeRecipe from "./ClaudeRecipe.jsx"
+import IngredientsList from "./IngredientsList.jsx"
 
 export default function Main() {
-
-    /**
-     * Challenge: clean up our code!
-     * Let's make a couple new components to make things a
-     * little cleaner. (Notice: I'm not suggesting what we
-     * have now is bad or wrong. I'm mostly finding an excuse
-     * to get in some hands-on practice 🙂)
-     *
-     * 1. Move the entire recipe <section> into its own
-     *    ClaudeRecipe component
-     * 2. Move the list of ingredients <section> into its
-     *    own IngredientsList component.
-     *
-     * While you're considering how to structure things, consider
-     * where state is, think about if it makes sense or not to
-     * move it somewhere else, how you'll communicate between
-     * the parent/child components, etc.
-     *
-     * The app should function as it currently does when you're
-     * done, so there will likely be some extra work to be done
-     * beyond what I've listed above.
-     */
-
 
     const [ingredients, setIngredients] = useState([]);
     const ingredientListItems = ingredients.map(ingredient =>
@@ -60,19 +38,11 @@ export default function Main() {
                 <button>Add ingredient</button>
             </form>
             {ingredients.length > 0 ?
-            <section>
-                <h2>Ingredients on hand:</h2>
-                <ul className="ingredients-list" aria-live="polite">{ingredientListItems}</ul>
-                {ingredients.length > 3 &&
-                <div className="get-recipe-container">
-                    <div>
-                        <h3>Ready for a recipe?</h3>
-                        <p>Generate a recipe from your list of ingredients.</p>
-                    </div>
-                    <button onClick={getRecipe}>Get a recipe</button>
-                </div>
-                }
-            </section>
+            <IngredientsList
+                listItems={ingredientListItems}
+                returnRecipe={getRecipe}
+                numIngredients={ingredients.length}
+            />
             : null}
             {/* placeholder recipe contents */}
             {recipeShown &&
